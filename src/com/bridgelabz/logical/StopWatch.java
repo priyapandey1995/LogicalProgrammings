6e3aa7a0c4e48252e39b8df3ad7c877ee1579eb8
priya <priyapandey8884.blr@gmail.com>
2022-07-25 09:29:09 +0530

    [Priya]:Added Logical Programms

--- src/com/bridgelabz/logical/StopWatch.java
+++ src/com/bridgelabz/logical/StopWatch.java
@@ -1,4 +1,7 @@
 package com.bridgelabz.logical;
+
+import java.util.Scanner;
+
 /**
  * 
  * @author pande
@@ -11,20 +14,22 @@ public class StopWatch {
 	 */
 
 	public static void main(String[] args) {
+		
 		/**
 		 * Initializing elapsed=0
 		 * by using nanoTime() capturing the system start and stop time
 		 * calculating elapsed time by applying formula
 		 */
+		Scanner sc = new Scanner(System.in); 
 		long elapsed=0;
-		
+		System.out.println("Enter any charcater to start ");
+		char s = sc.next().charAt(0);   
 		long startTime = System.nanoTime(); 
 		System.out.println("Starting Time is" +" " +startTime);
-		
-		 
+		System.out.println("Enter any chareater to stop");
+		char c = sc.next().charAt(0);
 		long stopTime = System.nanoTime();
 		System.out.println("Stoping time is" + " " +stopTime);
-		
 		elapsed=stopTime-startTime;
 		System.out.println("elasped time is"+ " " + elapsed/1000 + "seconds");
 		
